655a06b60b4b1de2e80d0caeb6f3b0dc51b2f265
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2026-01-21 18:33:45 +0530

    Added new Assets

--- Assets/_3DStealthGame/scripts/PlayerMovementSimple.cs
+++ Assets/_3DStealthGame/scripts/PlayerMovementSimple.cs
@@ -11,6 +11,8 @@ public class PlayerMovement : MonoBehaviour
     public float turnSpeed = 20f;
 
     Rigidbody m_Rigidbody;
+
+    Animator m_Animator;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
 
@@ -18,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
     {
         m_Rigidbody = GetComponent<Rigidbody> ();
         MoveAction.Enable();
+        m_Animator = GetComponent<Animator> ();
     }
 
     void FixedUpdate ()
@@ -26,14 +29,23 @@ public class PlayerMovement : MonoBehaviour
         
         float horizontal = pos.x;
         float vertical = pos.y;
+
+        
         
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize ();
+
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);    
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool ("IsWalking", isWalking);
 
         Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation (desiredForward);
         
         m_Rigidbody.MoveRotation (m_Rotation);
-        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * walkSpeed * Time.deltaTime);
+       m_Rigidbody.MovePosition(
+    m_Rigidbody.position + transform.forward * walkSpeed * Time.fixedDeltaTime
+);
     }
 }
