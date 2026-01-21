30eb166713a176506ed9677d5829181ebda78877
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2026-01-21 20:33:41 +0530

    fixed sliding bug

--- Assets/_3DStealthGame/scripts/PlayerMovementSimple.cs
+++ Assets/_3DStealthGame/scripts/PlayerMovementSimple.cs
@@ -1,51 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     public InputAction MoveAction;
 
-    public float walkSpeed = 1.0f;
-    public float turnSpeed = 20f;
+    public float walkSpeed = 3f;
+    public float turnSpeed = 360f; // degrees per second
+    public float inputDeadzone = 0.02f;
 
     Rigidbody m_Rigidbody;
-
     Animator m_Animator;
+
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
 
-    void Start ()
+    void Awake()
     {
-        m_Rigidbody = GetComponent<Rigidbody> ();
-        MoveAction.Enable();
-        m_Animator = GetComponent<Animator> ();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Animator = GetComponentInChildren<Animator>(); // safe if Animator is on child
     }
 
-    void FixedUpdate ()
+    void OnEnable() => MoveAction.Enable();
+    void OnDisable() => MoveAction.Disable();
+
+    void FixedUpdate()
     {
-        var pos = MoveAction.ReadValue<Vector2>();
-        
-        float horizontal = pos.x;
-        float vertical = pos.y;
+        Vector2 input = MoveAction.ReadValue<Vector2>();
 
-        
-        
-        m_Movement.Set(horizontal, 0f, vertical);
-        m_Movement.Normalize ();
+        // Build movement vector from input
+        m_Movement.Set(input.x, 0f, input.y);
 
-        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);    
-        bool isWalking = hasHorizontalInput || hasVerticalInput;
-        m_Animator.SetBool ("IsWalking", isWalking);
+        // Deadzone to stop sliding/drift when no keys are pressed
+        if (m_Movement.sqrMagnitude < inputDeadzone * inputDeadzone)
+        {
+            if (m_Animator) m_Animator.SetBool("IsWalking", false);
+            return;
+        }
 
-        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
-        m_Rotation = Quaternion.LookRotation (desiredForward);
-        
-        m_Rigidbody.MoveRotation (m_Rotation);
-       m_Rigidbody.MovePosition(
-    m_Rigidbody.position + transform.forward * walkSpeed * Time.fixedDeltaTime
-);
+        m_Movement.Normalize();
+
+        // Optional: update animation
+        if (m_Animator) m_Animator.SetBool("IsWalking", true);
+
+        // Rotate toward movement direction
+        Vector3 desiredForward = Vector3.RotateTowards(
+            transform.forward,
+            m_Movement,
+            Mathf.Deg2Rad * turnSpeed * Time.fixedDeltaTime, // RotateTowards uses radians
+            0f
+        );
+
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+        m_Rigidbody.MoveRotation(m_Rotation);
+
+        // Move using the movement vector (NOT transform.forward)
+        m_Rigidbody.MovePosition(
+            m_Rigidbody.position + m_Movement * walkSpeed * Time.fixedDeltaTime
+        );
     }
 }
